c8471cabc1cf03ae947c118f3970c00acb22d301
Seth Messer <seth.messer@gmail.com>
2022-01-19 20:46:48 -0600

    chore: adds more fly.io specific changes
    - following https://hexdocs.pm/phoenix/fly.html

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,7 +26,7 @@ if config_env() == :prod do
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: [:inet6]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -40,11 +40,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  app_name = System.get_env("FLY_APP_NAME") ||
+    raise "FLY_APP_NAME not available"
+  host = "#{app_name}.fly.dev" || System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
+
   config :megalithic, MegalithicWeb.Endpoint,
-    url: [host: host, port: 443],
+    server: true,
+    url: [host: host, port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.

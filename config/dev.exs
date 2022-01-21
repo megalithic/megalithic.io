682bd62dbfdf4c1aec323b2b23de5805e2a5fbbb
Seth Messer <seth.messer@gmail.com>
2022-01-20 20:53:07 -0600

    chore: initial commit of all the yarn pnp stuff and some binaries

--- config/dev.exs
+++ config/dev.exs
@@ -59,7 +59,8 @@ config :megalithic, MegalithicWeb.Endpoint,
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/megalithic_web/(live|views)/.*(ex)$",
-      ~r"lib/megalithic_web/templates/.*(eex)$"
+      ~r"lib/megalithic_web/templates/.*(eex)$",
+      ~r"posts/*/.*(md)$"
     ]
   ]
 

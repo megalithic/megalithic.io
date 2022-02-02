4c399d66d93a1b3d28f2585b8adda5d30943f45f
Seth Messer <seth.messer@gmail.com>
2022-02-02 17:02:06 -0600

    fix: adds check_origin entry

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,7 +54,8 @@ if config_env() == :prod do
       transport_options: [socket_opts: [:inet6]]
     ],
     url: [scheme: "https", host: host, port: 443],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: ["megalithic.fly.dev", "//*.megalithic.io"]
 
   # server: true,
   # url: [host: host, port: 80],

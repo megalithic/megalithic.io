6633af6ae13b348dd6d1600d70776e5e4d25a1a8
Seth Messer <seth.messer@gmail.com>
2022-02-02 09:03:34 -0600

    chore: adds highlight.js

---
+++ assets/vendor/syntax.js
@@ -0,0 +1,40 @@
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import shell from "highlight.js/lib/languages/shell";
+import bash from "highlight.js/lib/languages/bash";
+import erb from "highlight.js/lib/languages/erb";
+import ruby from "highlight.js/lib/languages/ruby";
+import vim from "highlight.js/lib/languages/vim";
+import yaml from "highlight.js/lib/languages/yaml";
+import json from "highlight.js/lib/languages/json";
+import diff from "highlight.js/lib/languages/diff";
+import xml from "highlight.js/lib/languages/xml";
+import sql from "highlight.js/lib/languages/sql";
+import lua from "highlight.js/lib/languages/lua";
+import markdown from "highlight.js/lib/languages/markdown";
+
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("shell", shell);
+hljs.registerLanguage("bash", bash);
+hljs.registerLanguage("eex", erb);
+hljs.registerLanguage("ruby", ruby);
+hljs.registerLanguage("vim", vim);
+hljs.registerLanguage("yaml", yaml);
+hljs.registerLanguage("json", json);
+hljs.registerLanguage("diff", diff);
+hljs.registerLanguage("html", xml);
+hljs.registerLanguage("sql", sql);
+hljs.registerLanguage("lua", lua);
+hljs.registerLanguage("markdown", markdown);
+
+window.highlightAll = function (where = document) {
+  where.querySelectorAll("pre code").forEach((block) => {
+    const lang = block.getAttribute("class");
+    if (lang && lang !== "makeup elixir") {
+      const { value: value } = hljs.highlight(block.innerText, { language: lang });
+      block.innerHTML = value;
+    }
+  });
+};
+
+window.highlightAll();
